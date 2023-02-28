23dacc52dda5b6d1907fbeff4f2b907d080a0a14
WardAbdrabou <wardabdrabou123@gmail.com>
2023-02-28 14:40:31 +0200

    change log

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-z8vx3mkp.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://localhost:3000/api/v0",
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-z8vx3mkp.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://localhost:3000/api/v0'
 };
 
 /*
