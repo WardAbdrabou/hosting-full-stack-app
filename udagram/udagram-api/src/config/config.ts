a0010127181f9d7b8963a91a7c0a27def56c8dcd
WardAbdrabou <wardabdrabou123@gmail.com>
2023-03-02 01:44:29 +0200

    update1

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -18,8 +18,8 @@ export const config = {
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  //myAccessKeyId : process.env.AWS_ACCESS_KEY_ID,
-  //mySecretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
+  myAccessKeyId : process.env.AWS_ACCESS_KEY_ID,
+  mySecretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
