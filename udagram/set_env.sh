9f34410344cb3e3170747c70271add678eec39d5
WardAbdrabou <wardabdrabou123@gmail.com>
2023-03-03 17:24:30 +0200

    first commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=123
+export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST="database-1.cuxj8aehanbc.us-east-1.rds.amazonaws.com"
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::ward-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=postgresql://postgres:postgres@database-1.cuxj8aehanbc.us-east-1.rds.amazonaws.com:5432/postgres
